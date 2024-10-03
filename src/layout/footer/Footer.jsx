28a35db860e7bbcce0eeb279566b70a382ef54ad
rabeast <39444732+rabeast@users.noreply.github.com>
2024-10-03 09:48:52 +0300

    2.4.2 бага после 2.4.1

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -3,7 +3,7 @@ import './footer.css'
 const Footer = () => (
     <footer id='footer' className="page-footer blue-grey darken-4 footer-content">
         <div className="footer-left">
-            <div>Чётные дни: Павел Воробьёв</div>
+            <div>Чётные дни: Илья Катухов</div>
             <div>Нечётные дни: Иван Супрун</div>
         </div>
 
