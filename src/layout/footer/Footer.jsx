f7aa5ee49210dff0fbc02a5e60c9fbe5b29749dd
rabeast <saiforain@gmail.com>
2024-12-15 10:22:25 +0300

    3.1.4 выход с отпуска Катухов

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -3,8 +3,8 @@ import './footer.css'
 const Footer = () => (
     <footer id='footer' className="page-footer blue-grey darken-4 footer-content">
         <div className="footer-left">
-            {/* <div>Чётные дни: Илья Катухов</div> */}
-            <div>Чётные дни: Павел Воробьёв</div>
+            <div>Чётные дни: Илья Катухов</div>
+            {/* <div>Чётные дни: Павел Воробьёв</div> */}
             <div>Нечётные дни: Иван Супрун</div>
         </div>
 
